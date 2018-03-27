5a9ebd68ddd278c77fa2a09859eb9b14d18b9b1a
Bloodline <tamasboldizsar23@gmail.com>
2018-03-27 14:54:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bloodline/forgeofempiresfpcalculator/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.bloodline.forgeofempiresfpcalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
